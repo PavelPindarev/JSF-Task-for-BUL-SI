73c90b60e83ecf9f30fa39b0aeaa3111909d8830
PavelPindarev <pavel09.1970@abv.bg>
2023-10-15 19:04:29 +0300

    Initial commit

---
+++ src/main/java/bg/bulsi/model/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package bg.bulsi.model.entity;
+
+
+import lombok.*;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
